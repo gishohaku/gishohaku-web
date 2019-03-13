f59a16d803f625e19b4a41fdbaad2a9fb82bc04a
mottox2 <y.mottox2@gmail.com>
2019-03-14 01:10:03 +0900

    Add minireset.css and emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+
+import 'minireset.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,22 @@
 import React from "react"
+import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import logo from "../images/logo.png"
+
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1 css={css`
+      font-weight: bold;
+    `}>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <img src={logo}/>
     </div>
   </Layout>
 )
