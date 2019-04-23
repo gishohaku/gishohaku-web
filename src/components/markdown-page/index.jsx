2e7606b7f24c1b043f879de57da816c7bfff15e1
mottox2 <y.mottox2@gmail.com>
2019-04-23 14:21:16 +0900

    Tweak style

--- src/components/markdown-page/index.jsx
+++ src/components/markdown-page/index.jsx
@@ -11,6 +11,67 @@ const section = css`
   padding: 32px 0 48px;
 `
 
+const contents = css`
+  h2 {
+    font-size: 28px;
+    margin-top: 48px;
+    margin-bottom: 24px;
+    font-weight: 600;
+  }
+
+  h3 {
+    font-size: 20px;
+    margin: 32px 0 24px;
+    font-weight: 600;
+  }
+
+  h2 + h3 {
+    margin-top: 0;
+  }
+
+  ul,
+  ol {
+    padding-left: 24px;
+  }
+
+  li {
+    margin: 10px 0;
+  }
+
+  ul > li {
+    list-style-type: disc;
+  }
+
+  li > p {
+    margin: 0;
+  }
+
+  table {
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    width: 100%;
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin: 20px 0;
+    margin-bottom: 30px;
+  }
+
+  table tr:nth-child(odd) td {
+    background-color: #f9f9f9;
+  }
+
+  table tr th,
+  table tr td {
+    padding: 8px;
+    line-height: 1.6;
+    vertical-align: top;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+  }
+
+  table tr th {
+    white-space: nowrap;
+  }
+`
+
 const Top = ({ markdownPage }) => (
   <Layout>
     <SEO title="利用規約" keywords={[`同人誌即売会`, `技術同人誌`, `技書博`]} />
@@ -19,7 +80,10 @@ const Top = ({ markdownPage }) => (
         {markdownPage.frontmatter.title}
       </SectionHeader>
       <TextBlock>
-        <div dangerouslySetInnerHTML={{ __html: markdownPage.html }} />
+        <div
+          css={contents}
+          dangerouslySetInnerHTML={{ __html: markdownPage.html }}
+        />
       </TextBlock>
     </section>
   </Layout>
